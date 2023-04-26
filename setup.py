6a1a4bf21a8c2ffe1c7334060ef818d4fd799dbc
Shubham_asus <shubbh9@gmail.com>
2023-04-26 17:01:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'signLanguage',
+    version= '0.0.0',
+    author= 'Shaah-i',
+    author_email= 'shubbh9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
